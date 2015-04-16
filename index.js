2c789313feb93dfccad3fd536799e6b640f00570
Lee Tagg <mahkram@me.com>
2015-04-16 11:11:38 +1000

    Less Loggging

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var proxy = require('./lib/proxy'),
     log = require('./lib/log');
 
-function Server() {
+function Server(logging) {
+    if (logging == undefined) {
+        logging = true;
+    }
+
+    this.logging = logging;
     this.proxy = proxy;
 }
 
@@ -13,11 +18,16 @@ Server.prototype.resolveProxy = function(path, host) {
 Server.prototype.handle = function(req, res, next) {
 
     if (!this.proxy.shouldHandle(req.url)) {
-        log.http(req, res);
         return next();
     }
 
-    this.proxy.pipe(req, res, log.httpProxy.bind(log));
+    if (this.logging) {
+        this.proxy.pipe(req, res, log.httpProxy.bind(log));
+    } else {
+        this.proxy.pipe(req, res);
+    }
 };
 
-module.exports = new Server();
+module.exports = function(logging) {
+    return new Server(logging);
+};
